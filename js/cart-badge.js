98e935cf767a0cd81f3e53a9d7865ed36e67a6bb
avu-ua <avuua16@gmail.com>
2023-09-03 10:46:54 +0200

    dev

--- js/cart-badge.js
+++ js/cart-badge.js
@@ -12,7 +12,7 @@ const cartBadge = document.querySelector('.cart-badge');
 const cartCounter = document.querySelector('.cart-badge__counter');
 let itemsCount // початкова кількість замовленого товару невизначена
 
-function setOrderingListeners() {
+// function setOrderingListeners() {
     const addBtns = document.querySelectorAll('.add-btn'); // обираємо всі кнопки "Замовити / купити" (в т.ч. при прокрутці карусельки)
     addBtns.forEach((el) => {
         const itemIdElem = el.querySelector('.wineId')
@@ -27,11 +27,11 @@ function setOrderingListeners() {
             updateBasketJsonData(itemId, quantity) // Викликаємо функцію для оновлення JSON-стрінги для Корзини (містить пари "ID товара : кількість")
         });
     }); 
-}
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-    setOrderingListeners()
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     setOrderingListeners()
+// });
 
 function updateCartBadge() {
     cartCounter.textContent = itemsCount; // Оновлюємо вміст лічильника
